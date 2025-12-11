c13510f6deb1be5de18300a3ee999f690a6fdddd
Tahmid Hossain Jasbe <tahmidhossainjasbe@gmail.com>
2025-12-11 23:00:38 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,82 @@
-
 import streamlit as st
 import pickle
 import pandas as pd
+import requests
+import os
+
 st.title("Movie Recommender System")
 
-# movies_list=pickle.load(open('movies.pkl','rb'))
-# movies_list=movies_list['title'].values
 
-movies_dict=pickle.load(open('movies_dict.pkl','rb'))
-movies=pd.DataFrame(movies_dict)
+movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
 
-similarity=pickle.load(open('similarity.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
 
+API_KEY="8265bd1679663a7ea12ac168da84d2e8"
+
+if API_KEY is None:
+    st.error("⚠️ TMDB_API_KEY is not set in environment variables.")
+    st.stop()
+
+
+@st.cache_data(show_spinner=False)
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&language=en-US"
+    response = requests.get(url)
+    data = response.json()
+
+    if "poster_path" not in data or data["poster_path"] is None:
+        return "https://via.placeholder.com/500x750?text=No+Image"
+
+    return "https://image.tmdb.org/t/p/w500" + data["poster_path"]
+
+
+@st.cache_data(show_spinner=False)
 def recommend(movie):
-    movie_index=movies[movies['title']==movie].index[0]
-    distances=similarity[movie_index]
-    movies_list=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(
+        list(enumerate(distances)),
+        reverse=True,
+        key=lambda x: x[1]
+    )[1:6]
 
-    recommended_movies=[]
+    recommended_movies = []
+    recommended_posters = []
+
     for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
         recommended_movies.append(movies.iloc[i[0]].title)
-    return recommended_movies
+        recommended_posters.append(fetch_poster(movie_id))
 
+    return recommended_movies, recommended_posters
 
-selected_movies_name=st.selectbox(
-    'Choose your favorite movie genre:',
-    movies["title"].values
-    # movies_list
-    # ('Action', 'Comedy', 'Drama', 'Horror', 'Romance')
+selected_movie_name = st.selectbox(
+    'Choose your favorite movie:',
+    movies['title'].values
 )
 
-if st.button('Recommend'):
-    recommendations=recommend(selected_movies_name)
-    for i in recommendations:
-        st.write(i)
-    # st.write('You selected:', selected_movies_name)
+if st.button("Recommend"):
+    names, posters = recommend(selected_movie_name)
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
